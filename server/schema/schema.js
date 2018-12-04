3a1732488b8589d01414cce792d81bdc1119a65e
Yacine Benzerga <benzerga.yacine@hotmail.com>
2018-12-03 23:26:20 -0500

    seed mongodb and resolve for all endpoints

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -22,7 +22,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve(parent, args) {
-        /* return _.find(authors, { id: parent.authorId }); */
+        return Author.findById(parent.authorId);
       }
     }
   })
@@ -39,7 +39,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        /* return _.filter(books, { authorId: parent.id }); */
+        return Book.find({ authorId: parent.id });
       }
     }
   })
@@ -54,7 +54,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db/other source
-        return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       }
     },
     author: {
@@ -62,19 +62,19 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to get data from db/other source
-        return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       }
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return books;
+        return Book.find({});
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        return authors;
+        return Author.find({});
       }
     }
   }
