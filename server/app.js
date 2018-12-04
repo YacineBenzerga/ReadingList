703a466e3d0c9792d5d0d27de495d1dc5f3d864d
Yacine Benzerga <benzerga.yacine@hotmail.com>
2018-12-03 21:50:05 -0500

    added mongoose db connection through mlab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(
+  'mongodb://yacine:gql2334@ds051953.mlab.com:51953/gql-readinglist'
+);
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 app.use(
   '/graphql',
